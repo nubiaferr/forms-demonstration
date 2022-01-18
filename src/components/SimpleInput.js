815824307e7e0e57187fdb9943f7a3093076bd2b
nubiaferr <nubiaferrsvd@gmail.com>
2022-01-18 19:23:35 -0300

    Simple state validation & dynamic content/classes

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -3,6 +3,9 @@ import { useRef, useState } from "react";
 const SimpleInput = (props) => {
   const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('')
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true)
+
+  const nameInputClasses = enteredNameIsValid ? 'form-control' : 'form-control invalid'
 
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value)
@@ -11,15 +14,21 @@ const SimpleInput = (props) => {
   const formSubmissionHandler = event => {
     event.preventDefault()
     
-    const enteredValue = nameInputRef.current.value
-    console.log(enteredValue)
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return
+    }
+
+    setEnteredNameIsValid(true)
+
+    console.log(enteredName)
 
     setEnteredName('')
   }
 
   return (
     <form onSubmit={formSubmissionHandler}>
-      <div className='form-control'>
+      <div className={nameInputClasses}>
         <label htmlFor='name'>Your Name</label>
         <input 
         ref={nameInputRef} 
@@ -28,6 +37,7 @@ const SimpleInput = (props) => {
         onChange={nameInputChangeHandler}
         value={enteredName}
         />
+        {!enteredNameIsValid && <p className="error-text">Name must not be empty.</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>

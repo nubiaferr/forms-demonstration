a9132703b53d49565cbcbee7cbc8d6a2798f38d5
nubiaferr <nubiaferrsvd@gmail.com>
2022-01-18 19:58:36 -0300

    disabling submit button if !input with useEffect

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,36 +1,48 @@
-import {useState } from "react";
+import {useEffect, useState } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('')
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
+  const [formIsValid, setFormIsValid] = useState(false)
 
   const enteredNameIsValid = enteredName.trim() !== ''
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
+
+  useEffect(() => {
+    
+    if (enteredNameIsValid) {
+      setFormIsValid (true)
+    } else {
+      setFormIsValid(false)
+    }
+
+  }, [enteredNameIsValid])
+  
+  
+  const nameInputChangeHandler = event => {
+    setEnteredName(event.target.value)
+  }
+  
+  const nameInputBlurHandler = event => {
+    setEnteredNameTouched(true)
+  }
+  
+  const formSubmissionHandler = event => {
+    event.preventDefault()
+    
+    setEnteredNameTouched(true)
+    
+    if (!enteredNameIsValid) return
+    
+    console.log(enteredName)
+    
+    setEnteredName('')
+    setEnteredNameTouched(false)
+  }
   
   const nameInputClasses = nameInputIsInvalid 
   ? 'form-control invalid' 
   : 'form-control'
-
-  const nameInputChangeHandler = event => {
-    setEnteredName(event.target.value)
-  }
-
-  const nameInputBlurHandler = event => {
-    setEnteredNameTouched(true)
-  }
-
-  const formSubmissionHandler = event => {
-    event.preventDefault()
-
-    setEnteredNameTouched(true)
-
-    if (!enteredNameIsValid) return
-    
-    console.log(enteredName)
-
-    setEnteredName('')
-    setEnteredNameTouched(false)
-  }
 
   return (
     <form onSubmit={formSubmissionHandler}>
@@ -48,7 +60,7 @@ const SimpleInput = () => {
         )}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );

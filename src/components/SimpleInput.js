bfeb1d91ba77be834ef7287b66735013e598388b
nubiaferr <nubiaferrsvd@gmail.com>
2022-01-18 20:00:57 -0300

    refactoring without useEffect for disabling button

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,23 +1,11 @@
-import {useEffect, useState } from "react";
+import {useState } from "react";
 
 const SimpleInput = () => {
   const [enteredName, setEnteredName] = useState('')
   const [enteredNameTouched, setEnteredNameTouched] = useState(false)
-  const [formIsValid, setFormIsValid] = useState(false)
-
+ 
   const enteredNameIsValid = enteredName.trim() !== ''
   const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched
-
-  useEffect(() => {
-    
-    if (enteredNameIsValid) {
-      setFormIsValid (true)
-    } else {
-      setFormIsValid(false)
-    }
-
-  }, [enteredNameIsValid])
-  
   
   const nameInputChangeHandler = event => {
     setEnteredName(event.target.value)
@@ -60,7 +48,7 @@ const SimpleInput = () => {
         )}
       </div>
       <div className="form-actions">
-        <button disabled={!formIsValid}>Submit</button>
+        <button disabled={!enteredNameIsValid}>Submit</button>
       </div>
     </form>
   );
